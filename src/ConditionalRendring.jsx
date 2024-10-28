4d84bac73a3d115b1382f42f301e5366a0789520
Paras Bagri <108568290+parasbagri@users.noreply.github.com>
2024-10-28 18:59:08 +0000

    basic todo

--- src/ConditionalRendring.jsx
+++ src/ConditionalRendring.jsx
@@ -5,7 +5,7 @@ const ConditionalRendring = () =>{
     const [todos, setTodoes] = useState(['todo1', 'todo2'])
 
     const booleanMethod = () =>{
-        setTodoes(...todos, "One More"))
+        setTodoes(...todos, "One More")
     }
 
     return (
